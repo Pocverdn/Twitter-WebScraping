640fbc8359bdd921d33239537c76940176f8a0be
Pocverdn <ssanchezc5@eafit.edu.co>
2025-07-19 14:19:19 -0500

    Exel no funtional

--- DataMinning.py
+++ DataMinning.py
@@ -1,6 +1,5 @@
 import tweepy
-import pandas as pd
-
+import xlsxwriter
 bearer_token = 'Token here'
 
 client = tweepy.Client(bearer_token, wait_on_rate_limit=True)
@@ -16,9 +15,18 @@ def get_tweets(userId, number_of_tweets):
     for tweet in tweets.data:
         tweets_array.append(tweet)
 
-    #df = pd.DataFrame(tweets_array).T
-    #df.to_excel()
+    print(tweets_array)
+
+    print(type(tweets_array[0]))
+
+    #workbook = xlsxwriter.Workbook('tweets.xlsx')
+    #worksheet = workbook.add_worksheet()
+
+    #for row, data in enumerate(tweets_array):
+    #    worksheet.write(row, 0, data)
+
+    #workbook.close()
 
 
 if __name__ == '__main__':
-    get_tweets(2622731, 50)
+    get_tweets(2622731, 5)
