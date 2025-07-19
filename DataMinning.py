0bba4b055e56c37f3ed258bc3c0b77b2bb1802b9
Pocverdn <ssanchezc5@eafit.edu.co>
2025-07-19 17:03:40 -0500

    Exel

--- DataMinning.py
+++ DataMinning.py
@@ -1,6 +1,6 @@
 import tweepy
 import xlsxwriter
-bearer_token = 'Token here'
+bearer_token = 'Bearer_token Here'
 
 client = tweepy.Client(bearer_token, wait_on_rate_limit=True)
 
@@ -13,19 +13,17 @@ def get_tweets(userId, number_of_tweets):
     tweets_array=[]
 
     for tweet in tweets.data:
-        tweets_array.append(tweet)
+        tweets_array.append(str(tweet))
 
     print(tweets_array)
 
-    print(type(tweets_array[0]))
+    workbook = xlsxwriter.Workbook('tweets.xlsx')
+    worksheet = workbook.add_worksheet()
 
-    #workbook = xlsxwriter.Workbook('tweets.xlsx')
-    #worksheet = workbook.add_worksheet()
+    for row, data in enumerate(tweets_array):
+        worksheet.write(row, 0, data)
 
-    #for row, data in enumerate(tweets_array):
-    #    worksheet.write(row, 0, data)
-
-    #workbook.close()
+    workbook.close()
 
 
 if __name__ == '__main__':
